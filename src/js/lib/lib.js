89491abe613ea681a9ec4c7f83da1888f1185243
Alex <cigan@ua.fm>
2020-10-22 17:00:35 +0300

    lesson 10, add components for modal windows and a method to show and close a modal window

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -5,6 +5,7 @@ import './modules/handlers';
 import './modules/actions';
 import './modules/effects';
 import './components/dropdown';
+import './components/modal';
 
 
 export default $; // таким образом разширяем функционал $, добавляя методы с display...
--- src/js/main.js
+++ src/js/main.js
@@ -38,26 +38,26 @@ import $ from './lib/lib';
 // console.log($('button').fadeOut(1800));            // скрываем (делаем прозрачными) элементы в течении 18 сек. 
 // console.log($('button').fadeIn(1800));            // показываем элементы в течении 18 сек.
 
-$('#first').on('click', () => {
-    $('div').eq(1).fadeToggle(800);
-});
+// $('#first').on('click', () => {
+//     $('div').eq(1).fadeToggle(800);
+// });
 
-$('[data-count="second"]').on('click', () => {
-    $('div').eq(2).fadeToggle(800);
-});
+// $('[data-count="second"]').on('click', () => {
+//     $('div').eq(2).fadeToggle(800);
+// });
 
-$('button').eq(2).on('click', () => {
-    $('.w-500').fadeToggle(800);
-});
+// $('button').eq(2).on('click', () => {
+//     $('.w-500').fadeToggle(800);
+// });
 
-$('.wrap').html(
-    `<div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
-        <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
-            <a href="#" class="dropdown-item">Action</a>
-            <a href="#" class="dropdown-item">Action #2</a>
-            <a href="#" class="dropdown-item">Action #3</a>
-        </div>
-    </div>`
-);
-$('.dropdown-toggle').dropdown();
+// $('.wrap').html(
+//     `<div class="dropdown">
+//         <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+//         <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+//             <a href="#" class="dropdown-item">Action</a>
+//             <a href="#" class="dropdown-item">Action #2</a>
+//             <a href="#" class="dropdown-item">Action #3</a>
+//         </div>
+//     </div>`
+// );
+// $('.dropdown-toggle').dropdown();
