33fb3895236e70cc2e45fd9778effb6c16a1c3e5
Alex <cigan@ua.fm>
2020-10-22 14:57:35 +0300

    lesson 8, add method fadeToggle to hide elements when they show and vice versa

--- src/js/lib/modules/effects.js
+++ src/js/lib/modules/effects.js
@@ -56,3 +56,30 @@ $.prototype.fadeOut = function (dur, fin) {
 
     return this;
 };
+
+$.prototype.fadeToggle = function (dur, display, fin) {
+    for (let i = 0; i < this.length; i++) {
+        if (window.getComputedStyle(this[i]).display === 'none') { // Computed - стили, которые применились, не только инлайн стили
+            this[i].style.display = display || 'block';     // если display не передан, тогда block. Это старый способ записи значения по умолчанию
+
+            const _fadeIn = (complection) => {
+                this[i].style.opacity = complection;
+            };
+
+            const ani = this.animateOverTime(dur, _fadeIn, fin);
+            requestAnimationFrame(ani);
+        } else {
+            const _fadeOut = (complection) => {
+                this[i].style.opacity = 1 - complection;
+                if (complection === 1) {
+                    this[i].style.display = 'none';     // если display не передан, тогда block. Это старый способ записи значения по умолчанию
+                }
+            };
+
+            const ani = this.animateOverTime(dur, _fadeOut, fin);
+            requestAnimationFrame(ani);
+        }
+    }
+
+    return this;
+};
